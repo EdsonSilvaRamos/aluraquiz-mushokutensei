e68e1c60fe1049f7ae4c4a56e027722fca0c46c6
Edson Silva Ramos <edsonsilvaramos@gmail.com>
2021-01-25 19:51:36 -0300

    Código ao final da primeira aula da imersão react

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,47 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import db from "../db.json";
+import QuizLogo from "../src/components/QuizLogo";
+import Widget from "../src/components/Widget";
+import QuizBackground from "../src/components/QuizBackground";
+import GitHubCorner from "../src/components/GitHubCorner";
+import Footer from "../src/components/Footer";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo/>    
+        <Widget>
+          <Widget.Header>
+            <h1>Alura Quiz Mushoku Tensei</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>O quanto você sabe sobre Mushoku Tensei?</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Quiz da galera</h1>
+
+            <p>Lorem ipsun dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/EdsonSilvaRamos"/>
+    </QuizBackground>
+  );
 }
